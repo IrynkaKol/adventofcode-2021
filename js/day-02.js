c29124ffd74a98cc807c8806de2b3e3e88e77bf0
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-09-04 02:39:26 +0200

    day 1, 2

--- js/day-02.js
+++ js/day-02.js
@@ -1,44 +1,33 @@
-let horizontalPosition = 0;
-let depth = 0;
-
-
-function processCommands () {
+function processCommands() {
   const inputElement = document.getElementById("command-input");
-  const commands = inputElement.value.split(/[,\n]/);
+  const commandString = inputElement.value;
+  const commands = commandString.split(/\s+/); // this line will split the command line into individual commands based on any whitespace characters, and you'll end up with an array of commands, where each element of that array will be a separate command for the code to handle
 
+  let horizontalPosition = 0;
+  let depth = 0;
 
-  depth = 0;
-  const upCommands = [];
-  const downCommands = [];
-
-  for (const command of commands) {
-    const trimmedCommand = command.trim(); //remove extra spaces from the line
-    const [action, value] = trimmedCommand.split(" ");
-    const numericValue = parseInt(value);
+  for (let i = 0; i < commands.length; i += 2) {
+    // This is a for loop that iterates through an array of commands. i is initialized to 0 and the loop will continue until i is less than the length of the commands array. i += 2 means that after each iteration we increment i by 2 to skip every other command. So this loop will process commands in pairs (action and value).
+    const action = commands[i];
+    const value = parseInt(commands[i + 1], 10); // we get the value from the command, which is located after the action. i + 1 points to the next element of the array, where the numerical value of the command is located. We use parseInt to convert this value to an integer (specifying base 10 for the number) and store it in the value variable.
 
     if (action === "forward") {
-      horizontalPosition += numericValue;
+      horizontalPosition += value;
     } else if (action === "down") {
-      downCommands.push(numericValue);
+      depth += value;
     } else if (action === "up") {
-      upCommands.push(numericValue);
+      depth -= value;
     }
   }
 
-  for (const value of upCommands) {
-    depth -= value;
-  }
-  for (const value of downCommands) {
-    depth += value;
-  }
-
-  document.getElementById("horizontal-position").textContent = horizontalPosition;
+  document.getElementById("horizontal-position").textContent =
+    horizontalPosition;
   document.getElementById("depth").textContent = depth;
   document.getElementById("result").textContent = horizontalPosition * depth;
 
-  inputElement.value = ""
+  inputElement.value = "";
 }
 
-document.getElementById("button").addEventListener("click", function() {
-  processCommands ()
+document.getElementById("button").addEventListener("click", function () {
+  processCommands();
 });
